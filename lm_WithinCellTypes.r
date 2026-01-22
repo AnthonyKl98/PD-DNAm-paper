0c4c9afec719cd8b462d42283470b8bd506c0d14
AnthonyKl98 <85745703+AnthonyKl98@users.noreply.github.com>
2026-01-22 11:18:11 +0000

    Tidying up
    Tidying up mainly data preparation section

--- lm_WithinCellTypes.r
+++ lm_WithinCellTypes.r
@@ -4,10 +4,9 @@
 ##
 ## Purpose of script: perform DNA methylation association analysis of Parkinson's cases vs controls in cell types separately
 ##
-## Author: Anthony Klokkaris (adapted from Eilis Hannon's script) 
+## Author: Anthony Klokkaris (adapted from Eilis Hannon) 
 ##
 ##---------------------------------------------------------------------#
-
 
 #----------------------------------------------------------------------#
 # DEFINE ANALYSIS FUNCTION
@@ -21,7 +20,6 @@ runEWAS<-function(row,QCmetrics){
   return(c(summary(nullCT)$coefficients["QCmetrics$PhenotypePD", c(1,2,4)]))  
 }
 
-
 #----------------------------------------------------------------------#
 # LOAD PACKAGES
 #----------------------------------------------------------------------#
@@ -34,10 +32,6 @@ library(dplyr)
 #----------------------------------------------------------------------#
 
 dataDir <- [path to data directory]
-
-#Cell type: NeuN+, SOX10+ or double negative
-cellType <- "NeuN+"    
-
 normData<-file.path(dataDir, "3_normalised/normalised.rdata")
 resPath<-file.path(dataDir, "4_analysis/EWAS") 
 
@@ -51,27 +45,25 @@ load(normData)
 QCmetrics <- QCmetrics %>% mutate(Phenotype = ifelse(Phenotype == "PD_GBA" | Phenotype == "PD_nonGBA", "PD", Phenotype))
 QCmetrics <- QCmetrics %>% mutate(Phenotype = ifelse(Phenotype == "Control_GBA" | Phenotype == "Control_nonGBA", "Control", Phenotype))
 
-#Exclude samples with failed CETYGO check
+#Exclude samples which failed CETYGO check
 QCmetrics <- QCmetrics[!(QCmetrics$Sample_ID %in% c("P94/05_DOUBLE NEG", "P11/17_SOX10 +", "P47/11_SOX10 +", "P24/17_SOX10 +", "P40/17_SOX10 +", "P2/11_SOX10 +", "P79/10_SOX10 +", "P72/12_DOUBLE NEG", "P16/12_SOX10 +", "P73/15_SOX10 +", "19870835_SOX10 +", "20000117_SOX10 +", "20050096_SOX10 +", "20174934_SOX10 +", "20174929_SOX10 +", "20040076_SOX10 +", "19990275_DOUBLE NEG")),]
 
+QCmetrics$Phenotype <- factor(QCmetrics$Phenotype)
+QCmetrics$Age <- as.numeric(QCmetrics$Age)
+QCmetrics$Sex <- factor(QCmetrics$Sex)
+QCmetrics$Institute <- factor(QCmetrics$Institute)
 
+#Cell type: NeuN+, SOX10+ or Double negative
+cellType <- "NeuN+"    
 print(paste0("running EWAS on ", cellType, " cell type..."))
+
 ## subset to cell type samples
 QCmetrics<-QCmetrics[which(QCmetrics$Cell_Type == cellType),]
 
 # subset beta matrix to analysis samples
-
 celltypeNormbeta<-celltypeNormbeta[,QCmetrics$Basename]
 celltypeNormbeta <- celltypeNormbeta[complete.cases(celltypeNormbeta),]
  
-QCmetrics$Phenotype <- factor(QCmetrics$Phenotype)
-QCmetrics$Cell_Type <- factor(QCmetrics$Cell_Type)
-QCmetrics$Age <- as.numeric(QCmetrics$Age)
-QCmetrics$Sex <- factor(QCmetrics$Sex)
-QCmetrics$Institute <- factor(QCmetrics$Institute)
-QCmetrics$Array_Plate <- factor(QCmetrics$Array_Plate)
-
-
 #----------------------------------------------------------------------#
 # INTITATE PARALLEL ENV
 #----------------------------------------------------------------------#
@@ -83,9 +75,13 @@ clusterExport(cl, list("runEWAS"))
 
 outtab<-matrix(data = parRapply(cl, celltypeNormbeta, runEWAS, QCmetrics), ncol = 3, byrow = TRUE)
 
+#----------------------------------------------------------------------#
+# LABEL AND SAVE RESULTS
+#----------------------------------------------------------------------#
+
 rownames(outtab)<-rownames(celltypeNormbeta)
 colnames(outtab)<-c("PD_coeff", "PD_SE", "PD_P")  
 
-save(outtab, file = file.path(resPath, paste0(cellType,"lm_results.rdata")))
+save(outtab, file = file.path(resPath, paste0(cellType,"_lm_results.rdata")))
+write.csv(outtab, file = file.path(resPath, paste0(cellType,"_lm_results.csv")))
 
-write.csv(outtab, file = file.path(resPath, paste0(cellType,"lm_results.csv")))
