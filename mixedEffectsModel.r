d1f845d52de6221cb19c386b2e5205434e10a37a
AnthonyKl98 <85745703+AnthonyKl98@users.noreply.github.com>
2026-01-23 14:04:09 +0000

    Update mixedEffectsModel.r

--- mixedEffectsModel.r
+++ mixedEffectsModel.r
@@ -62,8 +62,8 @@ load(normData)
 QCmetrics <- QCmetrics %>% mutate(Phenotype = ifelse(Phenotype == "PD_GBA" | Phenotype == "PD_nonGBA", "PD", Phenotype))
 QCmetrics <- QCmetrics %>% mutate(Phenotype = ifelse(Phenotype == "Control_GBA" | Phenotype == "Control_nonGBA", "Control", Phenotype))
 
-#Exclude samples which failed CETYGO check
-QCmetrics <- QCmetrics[!(QCmetrics$Sample_ID %in% c("P94/05_DOUBLE NEG", "P11/17_SOX10 +", "P47/11_SOX10 +", "P24/17_SOX10 +", "P40/17_SOX10 +", "P2/11_SOX10 +", "P79/10_SOX10 +", "P72/12_DOUBLE NEG", "P16/12_SOX10 +", "P73/15_SOX10 +", "19870835_SOX10 +", "20000117_SOX10 +", "20050096_SOX10 +", "20174934_SOX10 +", "20174929_SOX10 +", "20040076_SOX10 +", "19990275_DOUBLE NEG")),]
+#Exclude any samples which failed CETYGO check - add samples to exclude here
+QCmetrics <- QCmetrics[!(QCmetrics$Sample_ID %in% c("...")),]
 
 QCmetrics$Phenotype <- factor(QCmetrics$Phenotype)
 QCmetrics$Cell_Type <- factor(QCmetrics$Cell_Type)
@@ -96,4 +96,5 @@ rownames(outtab)<-rownames(celltypeNormbeta)
 colnames(outtab)<-c("PD_coeff", "PD_SE", "PD_P", "CellType_PD_P", "NeuN_PD_coeff", "NeuN_PD_SE","NeuN_PD_P", "SOX10_PD_coeff", "SOX10_PD_SE","SOX10_PD_P", "CellType_P","NeuN_coeff", "NeuN_SE", "NeuN_P", "SOX10_coeff", "SOX10_SE", "SOX10_P")  
 
 save(outtab, file = file.path(resPath, "mlm_results.rdata")) 
+
 write.csv(outtab, file = file.path(resPath, "mlm_results.csv"))
